862003f6c6163cafe71e48ea191e8d89d614b218
sourabhdesai <sourabhdesai@gmail.com>
2015-10-20 10:49:12 -0500

    changed ip to localhost in config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -22,7 +22,7 @@ module.exports = {
   },
 
   apiServer: {
-    ip: '127.0.0.1', // currently only have 1 VM...so web server & api server on same machine
+    ip: 'localhost', // currently only have 1 VM...so web server & api server on same machine
     port: 8080, // look at API server prod config
     context: '/api' // redirect calls to '/api/*' to API Server
   }
