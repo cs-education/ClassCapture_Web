1f3aa0b51a8aaf5f048f5c6928724842366a2fdf
sourabhdesai <sourabhdesai@gmail.com>
2015-10-19 20:40:06 -0500

    added back path rewriting

--- server/routes.js
+++ server/routes.js
@@ -12,9 +12,12 @@ var proxyMiddleware = require('http-proxy-middleware');
 module.exports = function(app) {
 
   // Proxy '/api/*' endpoint to API server
+  var rewriteTable = {};
+  rewriteTable[`${config.apiServer.context}`] = '/';
   var apiProxy = proxyMiddleware(`${config.apiServer.context}`, {
     'target': `http://${config.apiServer.ip}:${config.apiServer.port}`,
     'ws': true,
+    'pathRewrite': rewriteTable
   });
   
   app.use(apiProxy);
